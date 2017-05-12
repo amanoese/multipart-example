af70e085017d55b5ee75e0f3fdc3a77a8ab82ed2
amanoese <amanoese@gmail.com>
2017-05-12 16:42:55 +0900

    default rooting add

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const app = express();
 const upload = multer({ dest: 'uploaded' })
 
 app.use('/',express.static('src'));
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 app.post('/postFiles', upload.array('files'), function (req, res) {
   console.log(req.files);
   console.log(`files : ${req.files.length}`);
   res.send(`FILE-JSON:${JSON.stringify(req.files)}`);
   res.end();
+});
+
+app.get('/*', function (req, res) {
+  res.send('Hello World!');
 });
 
 app.listen(3000, function () {
